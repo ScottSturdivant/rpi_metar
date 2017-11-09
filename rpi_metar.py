b73f69622cd9e26e869a98f914afa0dabd5c6308
Scott Sturdivant <scott.sturdivant@gmail.com>
2017-11-09 13:42:18 -0700

    Convert to asyncio.

--- rpi_metar.py
+++ rpi_metar.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+import asyncio
 import requests
 import logging
 import re
 from enum import Enum
 from retrying import retry
+# from neopixel import Color
+def Color(red, green, blue, white = 0):
+    return (white << 24) | (red << 16)| (green << 8) | blue
 
 log = logging.getLogger()
 
@@ -40,13 +44,26 @@ AIRPORT_CODES = {
 
 URL = 'http://www.aviationweather.gov/metar/data?ids={airport_codes}&format=raw&hours=0&taf=off&layout=off&date=0'
 
+GREEN = Color(0, 255, 0)
+RED = Color(255, 0, 0)
+BLUE = Color(0, 0, 255)
+MAGENTA = Color(255, 0, 255)
+YELLOW = Color(255, 255, 0)
+BLACK = Color(0, 0, 0)
+
 
 class FlightCategory(Enum):
-    VFR = 'green'
-    IFR = 'red'
-    MVFR = 'blue'
-    LIFR = 'magenta'
-    UNKNOWN = 'yellow'
+    VFR = GREEN
+    IFR = RED
+    MVFR = BLUE
+    LIFR = MAGENTA
+    UNKNOWN = YELLOW
+
+
+TOGGLE = {
+    FlightCategory.UNKNOWN.value: BLACK,
+    BLACK: FlightCategory.UNKNOWN.value,
+}
 
 
 @retry(wait_exponential_multiplier=1000,
@@ -94,40 +111,74 @@ def get_flight_category(visibility, ceiling):
         return FlightCategory.LIFR
     raise ValueError
 
+class Dummy():
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def begin(self):
+        pass
+
+    def setPixelColor(self, pos, col):
+        pass
+
+    def show(self):
+        pass
 
 
+class LEDS(object):
 
-def main():
-    leds = Adafruit_NeoPixel(len(AIRPORT_CODES))
-    leds.begin()
+    def __init__(self):
+        # self._strip = Adafruit_NeoPixel(len(AIRPORT_CODES))
+        self._strip = Dummy()
+        self._strip.begin()
+        # A dictionary mapping LED positions to their current color value.
+        self._leds = {position: FlightCategory.UNKNOWN.value for position in AIRPORT_CODES}
+        self.display()
 
-    for position in AIRPORT_CODES:
-        leds.setPixelColor(position, FlightCategory.UNKNOWN.value)
-    leds.show()
 
+    def update_color(self, position, color):
+        self._leds[position] = color
+
+    async def display(self):
+        while True:
+            print('Displaying.')
+            for position, color in self._leds.items():
+                if color in TOGGLE:
+                    color = TOGGLE[color]
+                # Update the actual LED
+                self._strip.setPixelColor(position, color)
+                # And update our reference
+                self._leds[position] = color
+            self._strip.show()
+            await asyncio.sleep(0.5)
+
+
+async def refresh_metar_info(leds):
     while True:
+        print("Updating metar info.")
         try:
             info = get_metar_info()
         except:
             log.exception('Failed to retrieve metar info.')
             for position in AIRPORT_CODES:
-                leds.setPixelColor(position, FlightCategory.UNKNOWN.value)
-            leds.show()
-            time.sleep(60)
-            continue
+                leds.update_color(position, FlightCategory.UNKNOWN.value)
+            await asyncio.sleep(60)
 
         for position, code in AIRPORT_CODES.items():
             visibility, ceiling = get_visibility_and_ceiling(info.content.decode('utf-8'), code)
+            # print(code, visibility, ceiling)
             category = get_flight_category(visibility, ceiling)
-            leds.setPixelColor(position, category.value)
+            leds.update_color(position, category.value)
+        await asyncio.sleep(5 * 60)
 
-        leds.show()
-        time.sleep(5 * 60)
 
+def main():
+    leds = LEDS()
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(leds.display())
+    loop.create_task(refresh_metar_info(leds))
+    loop.run_forever()
 
 if __name__ == '__main__':
-    info = get_metar_info()
-    for position, code in AIRPORT_CODES.items():
-        visibility, ceiling = get_visibility_and_ceiling(info.content.decode('utf-8'), code)
-        category = get_flight_category(visibility, ceiling)
-        print('{}:\n\tVisibility: {}\n\tCeiling: {}\n\tCategory: {}'.format(code, visibility, ceiling, category))
+    main()
