10b9166eb67529d19577cd251c6efac68032e6eb
Scott Sturdivant <scott.sturdivant@gmail.com>
2018-04-11 10:47:36 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 setup(
     name='rpi_metar',
     py_modules=['rpi_metar'],
-    version='0.1.6',
+    version='0.1.7',
     description='Visualizing METAR data on a Raspberry Pi with LEDs.',
     keywords=['METAR', 'Raspberry Pi'],
     author='Scott Sturdivant',
