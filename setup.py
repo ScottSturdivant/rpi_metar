99d445a880faf2fe2f00662abedae88dacdd0408
Scott Sturdivant <scott.sturdivant@gmail.com>
2018-08-24 10:09:33 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ CLASSIFIERS = [
 
 setup(
     name=NAME,
-    version='0.2.0',
+    version='0.2.1',
     packages=PACKAGES,
     include_package_data=True,
     description='Visualizing METAR data on a Raspberry Pi with LEDs.',
