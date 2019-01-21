e47442a4fea2f84501122ec8d87422e71e0e7b62
Scott Sturdivant <scott.sturdivant@gmail.com>
2019-01-21 14:18:38 -0700

    Refactor encoder code.

--- rpi_metar/encoder.py
+++ rpi_metar/encoder.py
@@ -11,49 +11,48 @@ GPIO_B = 25
 
 class RotaryEncoder:
 
-    def __init__(self, callback, gpioA=GPIO_A, gpioB=GPIO_B):
-        self.lastGpio = None
-        self.gpioA = gpioA
-        self.gpioB = gpioB
+    def __init__(self, callback, gpio_a=GPIO_A, gpio_b=GPIO_B):
+        self.last_gpio = None
+        self.gpio_a = gpio_a
+        self.gpio_b = gpio_b
         self.callback = callback
 
-        self.levA = 0
-        self.levB = 0
+        self.level_a = 0
+        self.level_b = 0
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpioA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.gpioB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.gpio_a, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.gpio_b, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        GPIO.add_event_detect(self.gpioA, GPIO.BOTH, self._callback)
-        GPIO.add_event_detect(self.gpioB, GPIO.BOTH, self._callback)
+        GPIO.add_event_detect(self.gpio_a, GPIO.BOTH, self._callback)
+        GPIO.add_event_detect(self.gpio_b, GPIO.BOTH, self._callback)
 
     def destroy(self):
-        GPIO.remove_event_detect(self.gpioA)
-        GPIO.remove_event_detect(self.gpioB)
+        GPIO.remove_event_detect(self.gpio_a)
+        GPIO.remove_event_detect(self.gpio_b)
         GPIO.cleanup()
 
     def _callback(self, channel):
         level = GPIO.input(channel)
         log.debug('{channel} = {level}'.format(channel=channel, level=level))
-        if channel == self.gpioA:
-            self.levA = level
+        if channel == self.gpio_a:
+            self.level_a = level
         else:
-            self.levB = level
+            self.level_b = level
 
-        # Debounce.
-        if channel == self.lastGpio:
-            log.debug('debounced.')
+        if level != 1:
             return
 
         # When both inputs are at 1, we'll fire a callback. If A was the most recent pin set high,
         # it'll be forward, and if B was the most recent pin set high, it'll be reverse.
-        self.lastGpio = channel
-        log.debug('set lastGpio to {channel}'.format(channel=channel))
-        if channel == self.gpioA and level == 1:
-            if self.levB == 1:
+        if channel != self.last_gpio:  # debounce
+            self.last_gpio = channel
+            log.debug('set last_gpio to {channel}'.format(channel=channel))
+            if channel == self.gpio_a and self.level_b == 1:
                 log.debug('A is set and B was already set, callback(1)')
                 self.callback(1)
-        elif channel == self.gpioB and level == 1:
-            if self.levA == 1:
+                self.reset()
+            elif channel == self.gpio_b and self.level_a == 1:
                 log.debug('B is set and A was already set, callback(-1)')
                 self.callback(-1)
+                self.reset()
